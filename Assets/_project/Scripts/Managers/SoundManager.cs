7e2f8c5dcb88c7ac8ce2efd2a02a180d30566a70
Charles Carter <47723974+cmcrter@users.noreply.github.com>
2022-05-17 16:35:30 +0100

    Starting Sound Integration

--- Assets/_project/Scripts/Managers/SoundManager.cs
+++ Assets/_project/Scripts/Managers/SoundManager.cs
@@ -1,31 +1,94 @@
 ////////////////////////////////////////////////////////////
-// File: 
-// Author: 
-// Date Created: 
-// Last Edited By:
-// Date Last Edited:
-// Brief: 
+// File: SoundManager.cs
+// Author: Charles Carter
+// Date Created: 17/05/22
+// Last Edited By: Charles Carter
+// Date Last Edited: 17/05/22
+// Brief: A script to play sounds when needed
 //////////////////////////////////////////////////////////// 
 
+using System.Collections.Generic;
 using UnityEngine;
+
+//A class to hold necessary scene sounds
+[System.Serializable]
+public class SoundDetails
+{
+    public AudioClip clip;
+    public bool looping;
+    public float volume = 1f;
+}
 
 public class SoundManager : MonoBehaviour
 {
-    #region Public Fields
+    #region Variables
+
+    public static SoundManager instance;
+
+    [SerializeField]
+    private AudioSource musicSource;
+
+    [SerializeField]
+    private AudioSource sfxSource;
+
+    //This happens throughout all the UI so it's easier and is more effective to save it once
+    [SerializeField]
+    private AudioClip buttonClick;
+
+    [SerializeField]
+    private List<SoundDetails> sceneSoundsToPlay = new List<SoundDetails>();
+
     #endregion
- 
+
     #region Unity Methods
+
+    private void Awake()
+    {
+        if(instance)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
     void Start()
     {
-	
+        foreach(SoundDetails sound in sceneSoundsToPlay)
+        {
+            AudioSource source = gameObject.AddComponent<AudioSource>();
+            source.clip = sound.clip;
+            source.volume = sound.volume;
+            source.loop = sound.looping;
+            source.Play();
+        }
     }
- 
-    void Update()
-    {
-	
-    }
+
     #endregion
- 
-    #region Private Methods
+
+    #region Public Methods
+
+    public void PlaySFX(AudioClip sound)
+    {
+        sfxSource.clip = sound;
+        sfxSource.Play();
+    }
+
+    public void PlayButtonClick()
+    {
+        sfxSource.clip = buttonClick;
+        sfxSource.pitch = (Random.Range(0.1f, 0.9f));
+        sfxSource.Play();
+        sfxSource.pitch = 0.5f;
+    }
+
+    public void PlayMusic(AudioClip track)
+    {
+        musicSource.clip = track;
+        musicSource.Play();
+    }
+
     #endregion
 }
